81f98a66e68243d0e6f53f3caa4a8c46dca5f14f
Aikhan Zhashkeyev <spy.04@mail.ru>
2025-02-04 00:06:58 +0500

    fix(film-page): fix wrong film level evaluation

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -4,13 +4,13 @@ import duration from 'dayjs/plugin/duration';
 dayjs.extend(duration);
 
 export const getFilmLevel = (rating: number): FilmLevel | 'another level' => {
-  if (rating >= 0 && rating <= 3) {
+  if (rating >= 0 && rating < 3) {
     return FilmLevel.Bad;
-  } else if (rating > 3 && rating <= 5) {
+  } else if (rating >= 3 && rating < 5) {
     return FilmLevel.Normal;
-  } else if (rating > 5 && rating <= 8) {
+  } else if (rating >= 5 && rating < 8) {
     return FilmLevel.Good;
-  } else if (rating > 8 && rating < 10) {
+  } else if (rating >= 8 && rating < 10) {
     return FilmLevel.VeryGood;
   } else if (rating === 10) {
     return FilmLevel.Awesome;
