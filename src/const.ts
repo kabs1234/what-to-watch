1c9c2660c69374838d2dfecf97e13da158c524e3
Aikhan Zhashkeyev <spy.04@mail.ru>
2025-02-08 00:45:50 +0500

    fix(const): change MAX_GENRES_COUNT to 10 instead of 9

--- src/const.ts
+++ src/const.ts
@@ -4,7 +4,7 @@ export const STARTING_FILMS_COUNT = 8;
 
 export const ADDING_FILMS_COUNT = 8;
 
-export const MAX_GENRES_COUNT = 9;
+export const MAX_GENRES_COUNT = 10;
 
 export const RATING_STARS_COUNT = 10;
 
